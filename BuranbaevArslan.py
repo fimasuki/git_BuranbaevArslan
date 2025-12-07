738e7008e1872f2d9aa0cc70e86c5098e04a41d3
fimasuki <77119743+fimasuki@users.noreply.github.com>
2025-12-08 03:33:30 +0500

    Update BuranbaevArslan.py
    coomit from github

--- BuranbaevArslan.py
+++ BuranbaevArslan.py
@@ -5,6 +5,7 @@ def main():
     print("i don't know")
     print("Привет всем")
     print("Ура наконец-то все заработало УРА")
+    print("ИЗМЕНЯЮ ЧЕРЕЗ ГИТХАБ")
 if __name__ == "__main__":
         main()
 
